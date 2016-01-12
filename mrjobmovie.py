f6e9cf476fd9306a0c5d6f78ff92931ff84a2a1b
Mandeep <manminster@gmail.com>
2016-01-12 14:36:16 +0000

    added os.getcwd()

--- mrjobmovie.py
+++ mrjobmovie.py
@@ -2,6 +2,8 @@ from mrjob.job import MRJob
 from mrjob.step import MRStep
 from itertools import combinations
 from math import sqrt
+import os
+
 
 class MovieRecommender(MRJob):
 
@@ -12,7 +14,7 @@ class MovieRecommender(MRJob):
 	def load_movie_names(self):
 		self.movieNames = {}
 
-		with open("data/u.item") as f:
+		with open(os.getcwd().join("/data/u.item")) as f:
 			for line in f:
 				fields = line.split("|")
 				self.movieNames[int(fields[0])] = fields[1]
@@ -20,7 +22,7 @@ class MovieRecommender(MRJob):
 	def load_items(self):
 		self.genres ={}
 
-		with open("data/u.item") as f:
+		with open(os.getcwd().join("/data/u.item")) as f:
 			for line in f:
 				fields = line.split("|")
 				self.genres[int(fields[0])] = int(''.join(fields[5:23]))
